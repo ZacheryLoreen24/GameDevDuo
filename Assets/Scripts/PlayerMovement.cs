a5d33db0b95093cce240a859fef76b0061f67cff
Zachery Loreen <zrloreen@gmail.com>
2025-06-14 21:02:26 -0700

    bug Fixes + Sprinting
    Fixed issue when camera position at X = 90 or -90 player unable to jump / run. Also SPRINTING.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,47 +5,66 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
- 
+
     public float speed = 12f;
+    public float sprintMultiplier = 1.5f;
     public float gravity = -9.81f * 2;
     public float jumpHeight = 3f;
- 
+
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
- 
+
     Vector3 velocity;
- 
+
     bool isGrounded;
- 
+
     // Update is called once per frame
+    private float groundCheckOffset;
+
+    void Start()
+    {
+        groundCheckOffset = Mathf.Abs(groundCheck.localPosition.y);
+    }
+
     void Update()
     {
-        //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
- 
+        Vector3 checkPos = transform.position + Vector3.down * groundCheckOffset;
+        isGrounded = Physics.CheckSphere(checkPos, groundDistance, groundMask);
+
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
- 
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
- 
-        //right is the red Axis, foward is the blue axis
-        Vector3 move = transform.right * x + transform.forward * z;
- 
-        controller.Move(move * speed * Time.deltaTime);
- 
+
+        // use only the player's yaw rotation for movement so looking straight up or down keeps forward movement
+        float yaw = transform.eulerAngles.y;
+        Quaternion yawRotation = Quaternion.Euler(0f, yaw, 0f);
+        Vector3 right = yawRotation * Vector3.right;
+        Vector3 forward = yawRotation * Vector3.forward;
+
+        Vector3 move = right * x + forward * z;
+
+        float currentSpeed = speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed *= sprintMultiplier;
+        }
+        // keep purely horizontal movement
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
         //check if the player is on the ground so he can jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             //the equation for jumping
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
- 
+
         velocity.y += gravity * Time.deltaTime;
- 
+
         controller.Move(velocity * Time.deltaTime);
     }
 }
